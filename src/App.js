950259101d8b3906221d9a6f9748ec1f59a2dd3d
seanbenoit06 <153252679+seanbenoit06@users.noreply.github.com>
2025-06-19 00:58:18 -0700

    made changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
