5b44ae6f04633d5fec0edad9be8d3c3566ca01f4
seanbenoit06 <153252679+seanbenoit06@users.noreply.github.com>
2025-06-22 20:09:14 -0700

    added some initial content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Home from './Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,28 @@
 const Home = () => {
     return ( 
         <div className="Home">
-            <h1>this is Sean's page</h1>
+            <h1>Sean Benoit</h1>
+            <div className="About Me">
+                <p>
+                    Hello! I'm Sean Benoit, a rising 2nd year computer science student at the University of Washington.
+                    I'm interested in software engineering and have interests in machine learning, mathematics, and other 
+                    neat, applicable intersections of mathematics and software. In my free time, I love playing tennis, fitness,
+                    and enjoying the planet's natural beauty. 
+                </p>
+            </div>
+            <div className="Projects">
+                <p>
+                    Projects coming soon...
+                </p>
+            </div>
+            <div className="Resources">
+                <ul>
+                    <li>LinkedIn</li>
+                    <li>GitHub</li>
+                    <li>Email</li>
+                    <li>Resume</li>
+                </ul>
+            </div>
         </div>
      );
 }
